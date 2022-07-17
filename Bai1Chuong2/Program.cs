1045b2efcf2777f61f8655c88696d4e9ca84e2e3
Tuongvy Nguyễn <109215749+KhanhNguyen037565@users.noreply.github.com>
2022-07-17 12:36:45 +0700

    Practices Chapter 2

---
+++ Bai1Chuong2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*CODE: Nhập 3 chữ cái và hiển thị theo chiều ngược lại.*/
+using System;
+using System.Text;
+namespace ChuCai
+{
+    using System;
+    class ChuCai{
+        
+        public static int Main(string[] args){
+            string[] str= new string[3];
+            Console.OutputEncoding =Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Green;
+            for(int i=0; i< str.Length; i++){
+                Console.Write("Nhập chữ cái thứ {0}: ",i+1);
+                str[i]= Console.ReadLine();
+            }
+            Console.WriteLine("Dãy chữ trước khi đảo ngược: ");
+            for(int i=0; i< str.Length; i++){
+                Console.WriteLine("str[{0}]= {1}",i,str[i]);
+            }
+            Array.Reverse(str);
+            Console.WriteLine("Dãy chữ sau khi đảo ngược: ");
+            for(int i=0; i< str.Length; i++){
+                Console.WriteLine("str[{0}]= {1}",i,str[i]);
+            }
+            return 0;
+        }
+    }
+}
---
+++ Bai2Chuong2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text;
+
+using System;
+class Bai2{
+    public static void Main(string[] args){
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.OutputEncoding= Encoding.UTF8;
+        string UserName;
+        string PassWord;
+        int count=0;
+        do{
+            Console.WriteLine("Nhập User: ");
+            UserName= Console.ReadLine();
+            Console.WriteLine("Nhập PassWord: ");
+            PassWord= Console.ReadLine();
+            count ++;
+        }while(count !=3 && ( UserName !="Van Khanh" || PassWord !="02092002"));
+        if(count <=3 && ( UserName =="Van Khanh" && PassWord =="02092002")){
+            Console.WriteLine("Đăng nhập thành công!");
+            Console.Beep();
+        }
+        else{
+            Console.WriteLine("Tên đăng nhập hoặc mật khẩu sai.\nĐăng nhập thất bại");
+            Console.Beep();
+        }
+    }
+}
