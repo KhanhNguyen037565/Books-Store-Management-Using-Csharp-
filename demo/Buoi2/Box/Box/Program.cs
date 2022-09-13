c58ae9043d70bda16e324309c687383b543f976a
Tuongvy Nguyễn <109215749+KhanhNguyen037565@users.noreply.github.com>
2022-09-13 10:30:25 +0700

    up

---
+++ demo/Buoi2/Box/Box/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    
+        class Box
+        {
+            public double chieu_dai;
+            public double chieu_rong;
+            public double chieu_cao;
+        public void setChieuDai(double len)
+        {
+            chieu_dai = len;
+        }
+        public void setChieuCao(double hei)
+        {
+            chieu_dai = hei;
+        }
+        public void setChieuRong(double bre)
+        {
+            chieu_dai = bre;
+        }
+        public double tinhTheTich()
+        {
+            return chieu_dai * chieu_dai * chieu_cao;
+        }
+    }
+    
+}
---
+++ demo/Buoi2/testHaiFileCsharp/testCsharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testCsharp
+{
+    
+     public class TestCsharp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Class trong C#: ");
+            Console.WriteLine("________________");
+
+            Box box1 = new Box();
+            Box box2 = new Box();
+            double the_tich = 0.0;
+            // thong tin cua Box 1
+            box1.chieu_cao = 5.0;
+            box1.chieu_dai = 6.0;
+            box1.chieu_rong = 7.0;
+
+            // thong tin cua Box2
+            box2.chieu_cao = 10.0;
+            box2.chieu_dai = 16.0;
+            box2.chieu_rong = 17.0;
+
+            // tinh va in the tich cua box 1
+            the_tich = box1.chieu_cao * box1.chieu_dai * box1.chieu_rong;
+            Console.WriteLine("The tich cua box 1 la: {0}", the_tich);
+            // tinh va in the tich cua box 2
+            the_tich = box2.chieu_cao * box2.chieu_dai * box2.chieu_rong;
+            Console.WriteLine("The tich cua box 2 la: {0}", the_tich);
+            Console.ReadKey();
+
+        }
+    }
+}
