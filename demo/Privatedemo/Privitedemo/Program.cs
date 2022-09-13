eba212326706de078efa9695e67002143179728b
Tuongvy Nguyễn <109215749+KhanhNguyen037565@users.noreply.github.com>
2022-09-13 08:09:06 +0700

    up

---
+++ demo/Privatedemo/Privitedemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Privatedemo
+{
+    class rectangle
+    {
+        private double length;
+        private double width;
+
+        // cac phuong thuc
+        public void Acceptdetails()
+        {
+            Console.WriteLine("Nhap chieu dai: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap chieu rong: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Chieu dai: {0}", length);
+            Console.WriteLine("Chieu rong: {0}", width);
+            Console.WriteLine("Dien tich: {0}", GetArea());
+        }
+        static void Main(string[] args)
+        {
+            // tao doi tuong
+            rectangle r = new rectangle();
+            // thiet lap cac thuoc tinh
+            //r.length = 4.5;
+            //r.width = 3.5;
+            // goi phuong thuc
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ demo/Publicdemo/Privite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Privite
+{
+    class Rectangle
+    {
+        //cac bien thanh vien
+        public double length;
+        public double width;
+
+        //cac phuong thuc
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Chiue dai: {0}", length);
+            Console.WriteLine("Chieu rong: {0}", width);
+            Console.WriteLine("Dien tich: {0}", GetArea());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tinh dong goi trong C# ");
+            Console.WriteLine("______________________________");
+
+            // tao doi tuong rectangle
+            Rectangle r = new Rectangle();
+            //thiet lap cac thuoc tinh
+
+            r.length = 4.5;
+            r.width = 3.5;
+            // goi phuong thuc
+            r.Display();
+            Console.ReadKey();
+        }
+    }
+}
