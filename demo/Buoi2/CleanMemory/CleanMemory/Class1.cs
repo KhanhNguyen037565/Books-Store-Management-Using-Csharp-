a50f61eab5cc015d76f9f1b4c777be5c5552e797
Tuongvy Nguyễn <109215749+KhanhNguyen037565@users.noreply.github.com>
2022-09-16 09:18:20 +0700

    up

---
+++ demo/Buoi2/CleanMemory/CleanMemory/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMemory
+{
+    class Program
+    {
+        public static void Details()
+        {
+            // tao doi tuong Line bang constructor
+            Line line = new Line();
+            // thiet lap chieu dai cho duong
+            line.setChieuDai(7.0);
+            Console.WriteLine("Len ={0}", line.getChieuDai());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Destructor trong C#:");
+            Console.WriteLine("_______________");
+            Details();
+            GC.Collect(); // don dep bo nho
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ demo/Buoi2/CleanMemory/CleanMemory/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMemory
+{
+    class Line
+    {
+        private double chieuDai;
+        public Line()
+        {
+
+        }
+        public void setChieuDai(double len)
+        {
+            chieuDai = len;
+        }
+        public double getChieuDai()
+        {
+            return chieuDai;
+        }
+        ~Line()
+        {
+
+        }
+        
+
+    }
+}
