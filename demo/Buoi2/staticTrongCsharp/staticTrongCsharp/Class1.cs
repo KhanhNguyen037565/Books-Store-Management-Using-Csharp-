3b1213ff8e1446ea6f4ede3ed31a3f73066cb777
Tuongvy Nguyễn <109215749+KhanhNguyen037565@users.noreply.github.com>
2022-09-16 09:25:34 +0700

    update

---
+++ demo/Buoi2/staticTrongCsharp/staticTrongCsharp/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticTrongCsharp
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Phuong thuc static trong C#");
+            Console.WriteLine("_______________________________");
+
+            // tao cac doi tuong thanh vien static
+            ThanhVienStatic s = new ThanhVienStatic();
+            s.count();
+            s.count();
+            s.count();
+            Console.WriteLine("Gia tri cua num: {0}", ThanhVienStatic.getNum());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ demo/Buoi2/staticTrongCsharp/staticTrongCsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticTrongCsharp
+{
+    class ThanhVienStatic
+    {
+        public static int num; // thanh vien static
+        public void count()
+        {
+            num++;
+        }
+        // puong thuc static
+        public static int getNum()
+        {
+            return num;
+        }
+      
+    }
+}
