5606102c3f7c6d1792659b1678291af5c7ce1765
Tuongvy Nguyễn <109215749+KhanhNguyen037565@users.noreply.github.com>
2022-09-20 08:36:10 +0700

    up

---
+++ demo/Buoi3/demoTinhKeThua2/demoTinhKeThua2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoTinhKeThua2
+{
+    class HinhChuNhat
+    {
+        protected double chieuDai;
+        protected double chieuRong;
+        public HinhChuNhat(double l, double v)
+        {
+            chieuDai = l;
+            chieuRong = v;
+        }
+        public double tinhDienTich()
+        {
+            return chieuDai * chieuRong;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Chieu dai: {0}", chieuDai);
+            Console.WriteLine("Chieu rong: {0}", chieuRong);
+            Console.WriteLine("Dien tich: {0}", tinhDienTich());
+        }
+    }
+    class ChiPhiXayDung : HinhChuNhat
+    {
+        private double cost;
+        public ChiPhiXayDung(double l, double v) : base(l, v)
+        {
+
+        }
+        public double tinhChiPhi()
+        {
+            double chiPhi;
+            chiPhi = tinhDienTich() * 70;
+            return chiPhi;
+        }
+        public void hienThiThongTin()
+        {
+            base.Display();
+            Console.WriteLine("Chi phi: {0}", tinhChiPhi());
+        }
+    }
+    public class TestCsharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tinh ke thua trong C sharp:");
+            Console.WriteLine("___________________________");
+            // tao doi tuong chiphixaydung
+            ChiPhiXayDung t = new ChiPhiXayDung(4.5, 7.5);
+            t.hienThiThongTin();
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ demo/Buoi3/interface/interface/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    class Shape
+    {
+        protected int chieuRong;
+        protected int chieuDai;
+        public void setChieuRong(int r)
+        {
+            chieuRong = r;
+        }
+        public void setChieuDai(int d)
+        {
+            chieuDai = d;
+        }
+        public interface chiPhiSon
+        {
+            int tinhChiPhi(int dienTich);
+        }
+    class HinhChuNhat : Shape, chiPhiSon
+        {
+            public int tinhDienTich()
+            {
+                return (chieuDai * chieuRong);
+            }
+            public int tinhChiPhi(int dienTich)
+            {
+                return dienTich * 70;
+            }
+        }
+    public class TestCsharp
+        {
+            public static void Main(string[] args)
+            {
+                HinhChuNhat hcn = new HinhChuNhat();
+                int dienTich;
+                hcn.setChieuRong(5);
+                hcn.setChieuDai(7);
+                dienTich = hcn.tinhDienTich();
+
+                // in dien tich va chi phi
+                Console.WriteLine("Tong dien tich: {0}", hcn.tinhDienTich());
+                Console.WriteLine("Tong chi phi son: {0}", hcn.tinhChiPhi(dienTich));
+                Console.ReadKey();
+            }
+        }
+    }
+}
